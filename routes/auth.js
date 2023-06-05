3f04414caeb4410452b35acbbc6f168dd1e6920f
Jakob Rössner <jakob.roessner@outlook.de>
2023-06-05 19:00:38 +0200

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,7 @@ const authService = require('../services/auth.service');
 router.post('/register', async (req, res) => {
   const { email, password } = req.body;
   if (Buffer.byteLength(password, 'utf8') > 72) {
-    return res.status(400).json({ message: 'Password must be at most 72 bytes' });
+    return res.status(400).json({ message: 'Password to long (password must be at most 72 bytes)' });
   }
   try {
     const userData = await authService.register({ email, password });
